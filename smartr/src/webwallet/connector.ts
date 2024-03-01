bc5dfb5b065b9187e3a260ae71598ddcbe7c63ed
0xknwn <145777008+0xknwn@users.noreply.github.com>
2024-03-01 16:42:46 +0100

    initialize starknetWindowObject for smartr

---
+++ smartr/src/webwallet/connector.ts
@@ -0,0 +1,52 @@
+import EventEmitter from "eventemitter3"
+import { StarknetWindowObject, StarknetChainId } from "get-starknet-core"
+
+/** Connector icons, as base64 encoded svg. */
+export type ConnectorIcons = {
+  /** Dark-mode icon. */
+  dark?: string
+  /** Light-mode icon. */
+  light?: string
+}
+
+/** Connector data. */
+export type ConnectorData = {
+  /** Connector account. */
+  account?: string
+  /** Connector network. */
+  chainId?: StarknetChainId
+}
+
+/** Connector events. */
+export interface ConnectorEvents {
+  /** Emitted when account or network changes. */
+  change(data: ConnectorData): void
+  /** Emitted when connection is established. */
+  connect(data: ConnectorData): void
+  /** Emitted when connection is lost. */
+  disconnect(): void
+}
+
+export abstract class Connector extends EventEmitter<ConnectorEvents> {
+  /** Unique connector id. */
+  abstract get id(): string
+  /** Connector name. */
+  abstract get name(): string
+  /** Connector icons. */
+  abstract get icon(): ConnectorIcons
+
+  /** Whether connector is available for use */
+  abstract available(): boolean
+  /** Whether connector is already authorized */
+  abstract ready(): Promise<boolean>
+  /** Connect wallet. */
+  abstract connect(): Promise<ConnectorData>
+  /** Disconnect wallet. */
+  abstract disconnect(): Promise<void>
+  /** Get current account silently. Return null if the account is not authorized */
+  abstract account(): Promise<string | null>
+  /** Get current chain id. */
+  abstract chainId(): Promise<StarknetChainId>
+  /**  Connector StarknetWindowObject */
+  abstract get wallet(): StarknetWindowObject
+}
---
+++ smartr/src/webwallet/helpers/mapTargetUrlToNodeUrl.ts
@@ -0,0 +1,25 @@
+import { getRandomPublicRPCNode } from "./publicRcpNodes";
+
+export function mapTargetUrlToNodeUrl(target: string): string {
+  const publicRPCNode = getRandomPublicRPCNode();
+  try {
+    const { origin } = new URL(target);
+    if (origin.includes("localhost") || origin.includes("127.0.0.1")) {
+      return publicRPCNode.testnet;
+    }
+    if (origin.includes("hydrogen")) {
+      return publicRPCNode.testnet;
+    }
+    if (origin.includes("staging")) {
+      return publicRPCNode.mainnet;
+    }
+    if (origin.includes("argent.xyz")) {
+      return publicRPCNode.mainnet;
+    }
+  } catch (e) {
+    console.warn(
+      "Could not determine rpc nodeUrl from target URL, defaulting to mainnet"
+    );
+  }
+  return publicRPCNode.mainnet;
+}
